1940c153ee474826d7b2400b9d07198a4c3aca24
Glitch (nine-vigorous-joggers) <none>
2024-08-29 20:25:04 -0400

    making build

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
+  base: './', // This ensures relative paths
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000',
-    },
+  build: {
+    outDir: path.resolve(__dirname, '../docs'), // Adjust the path to your desired output directory
   },
 });
